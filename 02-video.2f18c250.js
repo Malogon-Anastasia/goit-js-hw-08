dd09324867ffad7929f00a199b1b6cb086de988c
Malogon-Anastasia <85022652+Malogon-Anastasia@users.noreply.github.com>
2021-10-26 18:52:55 +0000

    Deploying to gh-pages from @ Malogon-Anastasia/goit-js-hw-08@512eb93fe4b82bf18235076d8c61bc61f8c9cb0f 🚀

--- 02-video.2f18c250.js
+++ 02-video.2f18c250.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o="videoplayer-current-time",a=new e.default("vimeo-player");function l(){localStorage.getItem(o)&&(a.setCurrentTime(localStorage.getItem(o)),a.off("play",l))}function n(e){a.getCurrentTime().then(e=>localStorage.setItem(o,e))}a.on("play",l),a.on("timeupdate",(0,t.default)(n,1e3)),a.on("seeked",n);
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.4265d103.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.2f18c250.js.map
