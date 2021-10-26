026c80cc7179e397f1896157c66c69125f3ceee1
Malogon Anastasua <malogonanastasia@gmail.com>
2021-10-26 14:47:14 +0300

    update task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+// const PLAYER_TIME_ON_EXIT = 'videoplayer-current-time';
+
+// const player = new Player('vimeo-player');
+
+// player.on('play', onVideoPlay);
+
+// function onVideoPlay() {
+//   if (localStorage.getItem(PLAYER_TIME_ON_EXIT)) {
+//     player.setCurrentTime(localStorage.getItem(PLAYER_TIME_ON_EXIT));
+//     player.off('play', onVideoPlay);
+//   }
+// }
+
+// player.on('timeupdate', throttle(OnTimeUpdate, 1000));
+
+// player.on('seeked', OnTimeUpdate);
+
+// function OnTimeUpdate(data) {
+//   player.getCurrentTime().then(seconds => localStorage.setItem(PLAYER_TIME_ON_EXIT, seconds));
+// }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+ 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
 
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+let formData = {};
+refs.form.addEventListener('input', evt => {
+    formData[evt.target.name] = evt.target.value;
+    })
+
+
+populateForm();
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+    // const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY,  JSON.stringify(formData));
+}
+
+function populateForm() {
+const savedMessage = localStorage.getItem(STORAGE_KEY);
+if (savedMessage) {
+    // console.log(savedMessage);
+    refs.form.email.value = savedMessage;
+    refs.form.message.value = savedMessage;
+}
+}
